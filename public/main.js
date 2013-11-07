e3a09df61d3c29001c571bf062e4503c457a7469
Cori Allen <coriallen@gmail.com>
2013-11-07 08:13:53 -0500

    Add javascipt, main.js, and css stylesheet links into head of comments.html.

---
+++ public/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	get_comments();
+	
+});
+
+// $(document).ready(function(){
+// 	Blog.get_comments();
+// 	$('#comment-create-button').click(function(e){
+// 		e.precentDefault();
+// 		Blog.create_comment();
+// 	})
+// });
